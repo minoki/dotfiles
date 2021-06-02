72713ce814c79dc502fd0782def590562bca3ce0
ARATA Mizuki <minorinoki@gmail.com>
2021-06-02 10:01:54 +0900

    Update

--- emacs-init.el
+++ emacs-init.el
@@ -2,6 +2,7 @@
 (setq column-number-mode t)
 (setq show-trailing-whitespace t)
 (setq inhibit-startup-screen t)
+(setq-default indent-tabs-mode nil)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -19,21 +20,35 @@
 (add-hook 'c-mode-common-hook
 	  (lambda ()
 	    (setq c-basic-offset 4)
-	    (setq indent-tabs-mode nil)
-	    ))
-
-(add-hook 'js-mode-hook
-	  (lambda ()
-	    (setq indent-tabs-mode nil)
-	    ))
-
-(add-hook 'typescript-mode-hook
-	  (lambda ()
-	    (setq indent-tabs-mode nil)
 	    ))
 
 (add-hook 'lua-mode-hook
 	  (lambda ()
 	    (setq lua-indent-level 2)
-	    (setq indent-tabs-mode nil)
 	    ))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-haskell-server-path "~/.ghcup/bin/haskell-language-server-wrapper")
+ '(package-selected-packages
+   '(dumb-jump smartparens flycheck-pos-tip flycheck company-quickhelp company el-get leaf-keywords leaf lua-mode sml-mode proof-general yaml-mode lsp-ui lsp-haskell lsp-mode haskell-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+; haskell-mode
+(setenv "PATH" (concat (getenv "HOME") "/.local/bin" path-separator (getenv "PATH")))
+(setenv "PATH" (concat (getenv "HOME") "/.ghcup/bin" path-separator (getenv "PATH")))
+(set-variable 'haskell-mode-stylish-haskell-path "~/.local/bin/stylish-haskell")
+
+; https://github.com/emacs-lsp/lsp-haskell#emacs-configuration
+(require 'lsp)
+(require 'lsp-haskell)
+(add-hook 'haskell-mode-hook #'lsp)
+(add-hook 'haskell-literate-mode-hook #'lsp)
--- zshrc
+++ zshrc
@@ -15,6 +15,8 @@ if hostname | grep -qE '^ak'; then
    PROMPT="%{$fg[blue]%}%1~ %#%{$reset_color%} "
 fi
 
+setopt no_auto_remove_slash
+
 if [[ $(uname -s) = Darwin ]]; then
     alias Finder="open -a Finder.app"
     alias Emacs="open -a Emacs"
@@ -23,12 +25,30 @@ if [[ $(uname -s) = Darwin ]]; then
     else
         alias Firefox="open -a '/Applications/Firefox.app'"
     fi
-    alias brew="$HOME/homebrew/bin/brew"
     alias stree="/Applications/SourceTree.app/Contents/Resources/stree"
+    alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
+    export PATH="$HOME/homebrew/bin:$PATH"
+    export HOMEBREW_EDITOR=emacs
+    export INFOPATH="/opt/local/share/info:$INFOPATH"
+fi
+
+# TeX Live 2021
+if [ -d /usr/local/texlive/2021 ]; then
+    export MANPATH="/usr/local/texlive/2021/texmf-dist/doc/man:$MANPATH"
+    export INFOPATH="/usr/local/texlive/2021/texmf-dist/doc/info:$INFOPATH"
+    if [[ "$(uname -s)" = Darwin ]]; then
+	export PATH="/usr/local/texlive/2021/bin/universal-darwin:$PATH"
+    fi
 fi
 
 # Haskell / ghcup
 [ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env"
+
+test -r "$HOME/.opam/opam-init/init.zsh" && . "$HOME/.opam/opam-init/init.zsh" > /dev/null 2> /dev/null || true
+
+( which rlwrap sml > /dev/null 2>&1 ) && alias sml="rlwrap sml"
+( which rlwrap smlsharp > /dev/null 2>&1 ) && alias sml="rlwrap smlsharp"
+( which rlwrap ocaml > /dev/null 2>&1 ) && alias ocaml="rlwrap ocaml"
 
 if [ -d "$HOME/.local/bin" ]; then
     export PATH="$HOME/.local/bin:$PATH"
